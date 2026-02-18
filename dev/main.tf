bd767254ca6f3334234d397e0b8d7b5ef4904a00
Rexvanto <vrex@agfirst.com>
2026-02-18 18:35:24 +0530

    Add Auth0 Post Login Action

--- dev/main.tf
+++ dev/main.tf
@@ -2,3 +2,22 @@ resource "auth0_client" "test_app" {
   name     = "terraform-test-Rex"
   app_type = "regular_web"
 }
+resource "auth0_action" "add_claim" {
+  name    = "add-custom-claim"
+  runtime = "node18"
+  deploy  = true
+
+  code = <<EOT
+exports.onExecutePostLogin = async (event, api) => {
+  api.idToken.setCustomClaim(
+    "https://example.com/role",
+    "user"
+  );
+};
+EOT
+
+  supported_triggers {
+    id      = "post-login"
+    version = "v3"
+  }
+}
